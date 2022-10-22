f71cb781a5071b1399d62cb3b7c5d52e4758419a
Stoyan Haydushki <haydushki@uni-sofia.bg>
2022-10-22 21:47:45 +0300

    Add solutions for tasks 1 and 2

--- stackTasks.cpp
+++ stackTasks.cpp
@@ -1,13 +1,70 @@
+#include <stack>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-int task1( int number ) {
-  return 0;
+int task1(int number)
+{
+    stack<int> factorials;
+    factorials.push(1);
+
+    for (int i = 2; i <= number; ++i) {
+        int nextNumber = factorials.top() * i;
+        factorials.pop();
+        factorials.push(nextNumber);
+    }
+
+    return factorials.top();
 }
 
-bool task2 (const string& list) {
-    return false;
+bool task2(const string& list)
+{
+    stack<char> small; // ()
+    stack<char> medium; // []
+    stack<char> large; // {}
+
+    char current;
+    for (size_t i = 0; i < list.length(); i++) {
+        current = list[i];
+        switch (current) {
+        case '(': {
+            small.push(current);
+            break;
+        }
+        case ')': {
+            if (!small.empty()) {
+                small.pop();
+                break;
+            }
+            return false;
+        }
+        case '[': {
+            medium.push(current);
+            break;
+        }
+        case ']': {
+            if (!medium.empty()) {
+                medium.pop();
+                break;
+            }
+            return false;
+        }
+        case '{': {
+            large.push(current);
+            break;
+        }
+        case '}': {
+            if (!large.empty()) {
+                large.pop();
+                break;
+            }
+            return false;
+        }
+        }
+    }
+
+    return small.empty() && medium.empty() && large.empty();
 }
 
 bool task3(const vector<vector<int>>& g, unsigned from, unsigned to) {
